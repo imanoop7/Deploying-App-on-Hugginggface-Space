61297a0bb8544c6823438b83b595ea43b685f66b
imanoop7 <64773373+imanoop7@users.noreply.github.com>
2024-06-15 23:38:18 +0530

    dockerfile

---
+++ app.py
@@ -0,0 +1,19 @@
+from transformers import pipeline# Use a pipeline as a high-level helper
+from fastapi import FastAPI
+
+
+
+app = FastAPI()
+
+pipe = pipeline("text2text-generation", model="google/flan-t5-small")
+
+@app.get("/")
+def home():
+    return {"message":"Hello World"}
+
+@app.get("generate")
+def generate(text:str):
+    output = pipe(text)
+
+    return {"output":output[0]['generated_text']}
+
